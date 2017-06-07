33759ac4946e3cbf26328bae7e17611e3df9ab91
Picard Xie <picardxie@foxmail.com>
2017-06-07 09:46:52 +0800

    bvnj

--- skeleton-master/README.md
+++
@@ -1,6 +0,0 @@
-# skeleton
-A neat python web frame work<br>
-
-cd to the directory of server.py<br>
-then type:python server.py     <br>
-view localhost:8000/index/<br>
--- skeleton-master/urls.py
+++
@@ -1,6 +0,0 @@
-urls = [('^/index/$','func_index'),
-        ('^/comment/$','func_comment'),
-        ('^/environ/$','get_environ'),
-        ('^/post/$','post_test'),
-        ('^/majong/$','get_majong')
-        ]
